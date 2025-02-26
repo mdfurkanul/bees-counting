31df1725bee1f51dcd0afa9f8908f15d97e7b034
MD Furkanul Islam <mdfish191@studentmail.oru.se>
2025-02-27 00:11:56 +0100

    init project

---
+++ helping_module/helping_fns.py
@@ -0,0 +1,31 @@
+import h5py
+import torch
+import shutil
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor)*20
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+def save_net(fname, net):
+    with h5py.File(fname, 'w') as h5f:
+        for k, v in net.state_dict().items():
+            h5f.create_dataset(k, data=v.cpu().numpy())
+def load_net(fname, net):
+    with h5py.File(fname, 'r') as h5f:
+        for k, v in net.state_dict().items():        
+            param = torch.from_numpy(np.asarray(h5f[k]))         
+            v.copy_(param)
+            
+def save_checkpoint(state, is_best,task_id, filename='checkpoint.pth.tar'):
+    torch.save(state, task_id+filename)
+    if is_best:
+        shutil.copyfile(task_id+filename, task_id+'model_best.pth.tar')            
